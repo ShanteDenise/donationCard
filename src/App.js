912eb17ce6701e4f2925805a06100c831ff4b1f9
Shante <shante.austin19@gmail.com>
2021-03-15 13:41:11 -0400

    removed progress bar component and fix for button not showing on safari

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import "./App.scss";
-import ProgressBar from "./ProgressBar";
 
 const AppUI = () => {
     const goalAmountofCampaign = 5000;
@@ -8,9 +7,14 @@ const AppUI = () => {
     const [inputValue, setInputValue] = React.useState(0);
     const [completedPercentage, setCompletedPercent] = React.useState(0);
     const [numberOfDonors, setNumberOfDonors] = React.useState(0);
-    const [inValid, setInvalid] = React.useState(false)
-    let remainingGoalTotal = (goalAmountofCampaign - donateAmount);
+    const [inValid, setInvalid] = React.useState(false);
+    let remainingGoalTotal = goalAmountofCampaign - donateAmount;
 
+    const fillerStyles = {
+        height: "100%",
+        width: `${completedPercentage}%`,
+        backgroundColor: "#1acca4",
+    };
 
     const onChange = (e) => {
         const value = e.target.value;
@@ -19,22 +23,19 @@ const AppUI = () => {
         setInputValue(dollarAmount);
 
         if (dollarAmount >= 5) setInvalid(false);
-      
     };
-
 
     const onDonate = (e) => {
         e.preventDefault();
 
-        let totalDonated = (inputValue + donateAmount);
+        let totalDonated = inputValue + donateAmount;
         let progressToGoalpercentage =
             (totalDonated / goalAmountofCampaign) * 100;
 
-        if (inputValue < 5){
+        if (inputValue < 5) {
             setInvalid(true);
-          return
+            return;
         }
-
 
         if (inputValue <= remainingGoalTotal) {
             setDonateAmount(totalDonated);
@@ -51,21 +52,31 @@ const AppUI = () => {
     };
 
     return (
-        <>
             <div className="pageContainer">
-              <div className="visibleToolTip">
-                <div className="toolTipText"><span class="currency">$</span>
-<span className="remainingTotal">{remainingGoalTotal}</span> still needed to fund this project</div>
-              </div>
                 <div className="cardContainer">
-                    <ProgressBar completed={completedPercentage} />
+                <span className="visibleToolTip">
+                        <div className="toolTipText">
+                            <span class="currency">$</span>
+                            <span className="remainingTotal">
+                                {remainingGoalTotal}
+                            </span>{" "}
+                            still needed to fund this project
+                        </div>
+                    </span>
+
+                    <div className="progressBarContainer">
+                        <div style={fillerStyles}> </div>
+                    </div>
                     <div className="contentContainer">
                         <div className="fundingDateHeader">
                             Only four days left to fund this project
                         </div>
                         <div className="fundingDateSubHeader">
-                            Join the <span className="donorNumber">{numberOfDonors}</span> other
-                            donors who have already supported this project
+                            Join the{" "}
+                            <span className="donorNumber">
+                                {numberOfDonors}
+                            </span>{" "}
+                            other donors who have already supported this project
                         </div>
                         <div className="inputContainer">
                             <span className="currencyLabel">$</span>
@@ -75,13 +86,19 @@ const AppUI = () => {
                                 onChange={onChange}
                                 value={inputValue}
                             />
-                            <button disabled={inValid} onClick={onDonate}>Give Now</button>
+                            <button disabled={inValid} onClick={onDonate}>
+                                Give Now
+                            </button>
                         </div>
-                        {inValid && <div className="invalidText"> ⚠ Amount Must Be At Least $5</div> }
+                        {inValid && (
+                            <div className="invalidText">
+                                {" "}
+                                ⚠ Amount Must Be At Least $5
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
-        </>
     );
 };
 
